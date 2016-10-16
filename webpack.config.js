2850575459ad74ae6cc4e2ff7caaea803f20fb36
Allen <awhearry@gmail.com>
2016-10-16 13:13:56 -0400

    splitting bundles

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,10 @@ switch(process.env.npm_lifecycle_event) {
     		'process.env.NODE_ENV',
     		'production'
     	),
+    	parts.extractBundle({
+    		name: 'vendor',
+    		entries: ['react']
+    	}),
     	parts.minify(),
     	parts.setupCSS(PATHS.app)
     );

45047f4fd8a3deab79d338aaa24dbee09719f36b
Allen <awhearry@gmail.com>
2016-10-16 15:22:33 -0400

    hosting on github

--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,7 @@ switch(process.env.npm_lifecycle_event) {
     		devtool: 'source-map',
     		output: {
     			path: PATHS.build,
+    			publicPath: '/webpack-demo/',
     			filename: '[name].[chunkhash].js',
     			// This is used for require.ensure. The setup
     			// will work without but this is useful to set.
